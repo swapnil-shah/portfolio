28971813f51eafc6e288b6be060e82da424a2d7c
swapnil-shah <swapnilshah.me@gmail.com>
2021-04-05 13:21:24 -0400

    added dats json and arranged cards

---
+++ src/pug.config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  locals: {
+    data: {
+      favorites: [
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which ",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        },
+        {
+          tag: "Javascript",
+          imageSrc: "http://via.placeholder.com/640x360",
+          imageAlt: "image",
+          title: "Task Tracker",
+          description: "Hooks are new feature addition in react version 16.8 which allows you to use react features without having to write a class. Hooks don’t work inside classes",
+          href: "https://placeholder.com/"
+        }
+      ]
+    }
+  }
+};
